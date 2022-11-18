4a43fea04f8bb58a3ca20be5827aa631d1e4c9e0
Luhuertas <92084509+luhuertas@users.noreply.github.com>
2022-11-18 20:29:18 +0100

    PGA04-129 IT3 - UC04 - IMP - Implement Components - DS #DONE

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -3,22 +3,38 @@ package com.example.proyecto;
 import android.content.Intent;
 import static java.lang.Thread.sleep;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.example.proyecto.Json.JsonSingleton;
 import com.example.proyecto.Json.Montana;
 import com.example.proyecto.Json.Municipio;
+import com.example.proyecto.Room.DAO.UsuarioDAO;
+import com.example.proyecto.Room.Modelo.Usuario;
 import com.example.proyecto.databinding.ActivityMainBinding;
 import com.example.proyecto.ui.Eventos.CrearEventoActivity;
+import com.example.proyecto.ui.Localizaciones.LocalizacionesActivity;
 import com.example.proyecto.ui.inicio.InicioFragment;
 import com.google.gson.stream.JsonReader;
 
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.example.proyecto.Room.AppDatabase;
+import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -30,8 +46,11 @@ import androidx.room.Room;
 import com.google.gson.Gson;
 
 import java.io.InputStreamReader;
+import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,7 +93,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.localizaciones) {
+            // Actividad de localizaciones
+            startActivity(new Intent(this, LocalizacionesActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
 
     public void cargarJSON_en_Singleton(){
         // CÓDIGO DE MONTAÑAS
