cf8ce55700c314449dd89d0676c424eeefa2f7cc
Luis <luhertas@alumnos.unex.es>
2022-12-13 20:24:05 +0100

    Sonar 2

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import static java.lang.Thread.sleep;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
